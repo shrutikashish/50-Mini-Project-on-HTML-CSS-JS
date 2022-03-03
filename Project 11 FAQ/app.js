d733ded9af1d40842e0be9adca71f42877574bd5
DESKTOP-L14UOS9\Shruti <shruti.kashish8@gmail.com>
2022-03-03 15:22:19 +0530

    deleted

--- Project 11 FAQ/app.js
+++
@@ -1,8 +0,0 @@
-const toggles = document.querySelectorAll('.faq-toggle')
-
-toggles.forEach(toggle => {
-    toggle.addEventListener('click',()=> {
-        console.log(toggle.parentNode.classList)
-        toggle.parentNode.classList.toggle('active')
-    })
-})
